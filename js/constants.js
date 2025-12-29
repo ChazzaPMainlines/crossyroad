abde95bc7a728e5c377c26343aab7166bf8372ad
ChazzaPMainlines <charlie.pool@gmail.com>
2025-12-29 13:27:31 +0000

    Create constants.js
    Js V1

---
+++ js/constants.js
@@ -0,0 +1,19 @@
+export const CONFIG = {
+    GRID_SIZE: 40,
+    LANES_VISIBLE: 20,
+    LANE_WIDTH: 40,
+    PLAYER_SPEED: 0.2,
+    TYPES: {
+        GRASS: 'grass',
+        ROAD: 'road',
+        RIVER: 'river'
+    },
+    COLORS: {
+        PANTHER: 0x333333,
+        GRASS: 0x7cfc00,
+        ROAD: 0x444444,
+        RIVER: 0x0066ff,
+        CAR: [0xff0000, 0xffff00, 0xffffff, 0x00ff00],
+        LOG: 0x8b4513
+    }
+};
