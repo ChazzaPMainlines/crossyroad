3d8964f8d7ae4331383f07525b60dc2133bac180
ChazzaPMainlines <charlie.pool@gmail.com>
2025-12-29 13:42:27 +0000

    Update world.js
    World V2

--- js/world.js
+++ js/world.js
@@ -6,13 +6,13 @@ export class Lane {
     constructor(index, type) {
         this.index = index;
         this.type = type;
-        this.mesh = this.createMesh();
-        this.obstacles = [];
         this.direction = Math.random() > 0.5 ? 1 : -1;
-        this.speed = 1.5 + Math.random() * 2.5;
+        this.speed = 1.0 + Math.random() * 2.0;
+        this.obstacles = [];
+        this.mesh = this.createMesh();
 
-        if (type === CONFIG.TYPES.ROAD || type === CONFIG.TYPES.RIVER) {
-            this.setupObstacles();
+        if (type !== CONFIG.TYPES.GRASS) {
+            this.spawnObstacles();
         }
     }
 
@@ -24,25 +24,27 @@ export class Lane {
         const geometry = new THREE.PlaneGeometry(1000, CONFIG.GRID_SIZE);
         const material = new THREE.MeshPhongMaterial({ color });
         const mesh = new THREE.Mesh(geometry, material);
+        
         mesh.rotation.x = -Math.PI / 2;
+        // Position lanes sequentially along negative Z
         mesh.position.z = -this.index * CONFIG.GRID_SIZE;
         mesh.receiveShadow = true;
         return mesh;
     }
 
-    setupObstacles() {
-        const count = 3;
-        for (let i = 0; i < count; i++) {
+    spawnObstacles() {
+        for (let i = 0; i < 3; i++) {
             const obj = this.type === CONFIG.TYPES.ROAD ? createCar() : createLog();
             obj.position.z = -this.index * CONFIG.GRID_SIZE;
-            obj.position.x = (i * 300) - 450;
+            obj.position.x = (i * 350) - 500;
             this.obstacles.push(obj);
         }
     }
 
-    update(delta) {
+    update() {
         this.obstacles.forEach(obj => {
             obj.position.x += this.speed * this.direction;
+            // Wrap around
             if (this.direction > 0 && obj.position.x > 500) obj.position.x = -500;
             if (this.direction < 0 && obj.position.x < -500) obj.position.x = 500;
         });
